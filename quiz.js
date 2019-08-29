7d65b66607e77c209fc9067d88cdd6c17d05bded
Nethalin2 <cn73@CN0073.local>
2019-08-29 11:31:56 +0100

    added questions

--- quiz.js
+++ quiz.js
@@ -14,26 +14,26 @@ const scoreDiv = document.getElementById("scoreContainer");
 //create our questions
 let questions = [
     {
-        question: "What does HTML stand for?",
+        question: "In HTML, which attribute is used to specify that an input field must be filled out?",
         imgSrc : "img/html.png",
-        choiceA : "Hyper Text Markup Language",
-        choiceB : "Host Technology Makeup Light",
-        choiceC : "Hyper Time Medium Listener",
+        choiceA : "required",
+        choiceB : "placeholder",
+        choiceC : "validate",
         correct: "A"
     },{
-        question: "What does CSS stand for?",
+        question: "Where in an HTML document is the correct place to refer to an external style sheet?",
         imgSrc : "img/css.png",
-        choiceA : "Cider Sample Season",
-        choiceB : "Cascading Style Sheets",
-        choiceC : "Cruising Speed Sounds",
-        correct: "B"
+        choiceA : "In the head section",
+        choiceB : "At the end of the document",
+        choiceC : "In the body section",
+        correct: "A"
 
     },{
-        question: "What does JS stand for?",
+        question: "JavaScript is the same as Java",
         imgSrc : "img/js.png",
-        choiceA : "JamSlice",
-        choiceB : "JingleSleigh",
-        choiceC : "JavaScript",
+        choiceA : "Its similar but not the same",
+        choiceB : "True",
+        choiceC : "False",
         correct: "C"
     },{
         question: "Is JS a Truthy Language?",
@@ -43,7 +43,91 @@ let questions = [
         choiceC : "Yes",
         correct: "C"
 
+    } ,{
+        question: "Who is making Web standards?",
+        imgSrc : "img/html.png",
+        choiceA : "The World Wide Web Consortium",
+        choiceB : "Microsoft",
+        choiceC : "Google",
+        correct: "A"
+
+    },{
+        question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+        imgSrc : "img/html.png",
+        choiceA : "title",
+        choiceB : "src",
+        choiceC : "alt",
+        correct: "C"
+
+    },{
+        question: "In HTML, onblur and onfocus are",
+        imgSrc : "img/html.png",
+        choiceA : "Style attributes",
+        choiceB : "Event attributes",
+        choiceC : "HTML elements",
+        correct: "B"
+
+    },{
+        question: "The HTML canvas is used to:",
+        imgSrc : "img/html.png",
+        choiceA : "create draggable elements",
+        choiceB : "draw graphics",
+        choiceC : "manipulate data in MySQL",
+        correct: "B"
+
+    },{
+        question: "How to write an IF statement in JavaScript?",
+        imgSrc : "img/js.png",
+        choiceA : "if i = 5",
+        choiceB : "if i == 5 then",
+        choiceC : "if (i == 5)",
+        correct: "C"
+        
+
+    },{
+        question: "Which event occurs when the user clicks on an HTML element?",
+        imgSrc : "img/js.png",
+        choiceA : "onclick",
+        choiceB : "onchange",
+        choiceC : "onmouseclick",
+        correct: "A"
+    }, {
+        question: "How do you find the number with the highest value of x and y?",
+        imgSrc : "img/js.png",
+        choiceA : "ceil(x, y)",
+        choiceB : "Math.max(x, y) ",
+        choiceC : "Math.ceil(x, y)",
+        correct: "B"
+    }, {
+        question: "Which HTML attribute is used to define inline styles?",
+        imgSrc : "img/css.png",
+        choiceA : "Styles",
+        choiceB : "Style",
+        choiceC : "Font",
+        correct: "B"
+    }, { 
+        question: "Which property is used to change the background color?",
+        imgSrc : "img/css.png",
+        choiceA : "background-color",
+        choiceB : "bgcolor",
+        choiceC : "color",
+        correct: "A"
+    },{
+        question: "Which CSS property is used to change the text color of an element?",
+        imgSrc : "img/css.png",
+        choiceA : "text-color",
+        choiceB : "color",
+        choiceC : "fgcolor",
+        correct: "B"
+    },{
+        question: "Which CSS property controls the text size??",
+        imgSrc : "img/css.png",
+        choiceA : "font-size",
+        choiceB : "text-size",
+        choiceC : "text-style",
+        correct: "A"
     }
+
 
 ]
 
